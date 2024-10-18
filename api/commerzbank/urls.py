08203a2d66647b674d3ba41c1b14c6dbf6308d64
Kacper Kleczaj <kleczaj.kacper@gmail.com>
2024-10-18 21:53:42 +0200

    Upcoming payments part 2

--- api/commerzbank/urls.py
+++ api/commerzbank/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('contracts/', views.ContractView.as_view(), name='contracts'),
     path('register/', views.RegisterView.as_view(), name='register'),
     path('commerzbank/accounts/', views.AccountView.as_view(), name='accounts'),
+    path('upcoming-payments/', views.UpcomingPaymentView.as_view(), name='upcoming-payments'),
 ]
