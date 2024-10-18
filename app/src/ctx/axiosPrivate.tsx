6ec8d67e300a08f87554953092e2ba2f7f1801a8
januszmaro <janusz.miro152@gmail.com>
2024-10-19 00:10:00 +0200

    disable auth

--- app/src/ctx/axiosPrivate.tsx
+++ app/src/ctx/axiosPrivate.tsx
@@ -4,7 +4,7 @@ const axiosPrivate = axios.create({
     baseURL: 'http://localhost:8000/',
     headers: {
         'Content-Type': 'application/json',
-        'Authorization': `Bearer ${localStorage.getItem('token')}`
+        'Authorization': `Bearer ${localStorage.getItem('token') || ''}`
     }
 });
 
