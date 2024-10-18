1f1c0246a3509992bc658f93e94c1c46e25e5f33
pecor <251609@edu.p.lodz.pl>
2024-10-18 16:29:49 +0200

    rozpoczeciepracnadwidgetami

--- app/src/components/widgets/CardsList.tsx
+++ app/src/components/widgets/CardsList.tsx
@@ -1,8 +1,13 @@
+import { useState } from "react";
 
 const CardsList = () => {
+    const [cards, setCards] = useState([]);
+    
+    
     return (
         <div>
             <h1>CardsList</h1>
+
         </div>
     )
 }
