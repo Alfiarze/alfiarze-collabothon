31e299754525d3583b29814aa9e9e839f30a117f
januszmaro <janusz.miro152@gmail.com>
2024-10-19 12:30:59 +0200

    update

--- app/src/components/widgets/ReceiptWidget.tsx
+++ app/src/components/widgets/ReceiptWidget.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { Card, CardContent, Typography, List, ListItem, ListItemText, Box, IconButton } from '@mui/material';
 import { styled } from '@mui/system';
 import PhotoIcon from '@mui/icons-material/Photo';
