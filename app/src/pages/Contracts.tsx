4d55cfdf8810b17922b84dc3b16c0c6ec4dd60eb
pecor <251609@edu.p.lodz.pl>
2024-10-19 09:49:12 +0200

    added-contract-widget-to-contracts

--- app/src/pages/Contracts.tsx
+++ app/src/pages/Contracts.tsx
@@ -1,7 +1,10 @@
+import ContractsWidget from "../components/widgets/ContractsWidget"
+
 const Contracts = () => {
     return (
       <div>
         <h1>Contracts</h1>
+        <ContractsWidget/>
       </div>
     )
   }
