c8ef296715515560605ecaf4794e9a8a5e6d9d77
pecor <251609@edu.p.lodz.pl>
2024-10-19 19:14:03 +0200

    survey-questions-fix

--- app/src/pages/Actions.tsx
+++ app/src/pages/Actions.tsx
@@ -1,11 +1,13 @@
 import Roulette from "../components/widgets/Roulette"
 import BanksMap from "../components/widgets/BanksMap"
 import FullCalendar from "../components/widgets/FullCalendar"
+import StockExchange from "../components/widgets/StockExchange"
 
 const Actions = () => {
   return (
     <div>
       <h1>Actions</h1>
+      <StockExchange />
       <Roulette />
       <BanksMap />
       <FullCalendar />
